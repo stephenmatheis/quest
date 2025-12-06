01e4a8928a3ebb8d42619fc3008317aa73d932ee
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-12-06 17:30:21 -0500

    feat: control positions

--- src/components/World.tsx
+++ src/components/World.tsx
@@ -1,9 +1,7 @@
 import { useEffect, useRef } from 'react';
-import { CameraControls, CameraControlsImpl, Edges, Grid, PivotControls, Text3D } from '@react-three/drei';
+import { CameraControls, CameraControlsImpl, Edges, Grid, Text3D } from '@react-three/drei';
 import { Group, Shape, Vector3 } from 'three';
 import { useCameraControls } from '@/providers/CameraProvider';
-import { useThree } from '@react-three/fiber';
-import { m } from 'motion/react';
 
 const ASPECT_RATIO = 6 / 9;
 const WIDTH = 1;
@@ -55,18 +53,7 @@ function createRightShape() {
 
 function BoldText({ text, position }: { text: string; position: Vector3 }) {
     return (
-        <Text3D
-            position={position}
-            height={0.01}
-            size={LARGE_FONT_SIZE}
-            font="/fonts/Mono_Bold.json"
-            // curveSegments={32}
-            // bevelEnabled
-            // bevelSize={0.001}
-            // bevelThickness={0.001}
-            // lineHeight={0.5}
-            // letterSpacing={-0.06}
-        >
+        <Text3D position={position} height={0.01} size={LARGE_FONT_SIZE} font="/fonts/Mono_Bold.json">
             {text}
             <meshBasicMaterial color="#000000" />
         </Text3D>
@@ -75,18 +62,7 @@ function BoldText({ text, position }: { text: string; position: Vector3 }) {
 
 function RegularText({ text, position }: { text: string; position: Vector3 }) {
     return (
-        <Text3D
-            position={position}
-            height={0.01}
-            size={MEDIUM_FONT_SIZE}
-            font="/fonts/Mono_Regular.json"
-            // curveSegments={32}
-            // bevelEnabled
-            // bevelSize={0.001}
-            // bevelThickness={0.001}
-            // lineHeight={0.5}
-            // letterSpacing={-0.06}
-        >
+        <Text3D position={position} height={0.01} size={MEDIUM_FONT_SIZE} font="/fonts/Mono_Regular.json">
             {text}
             <meshBasicMaterial color="#000000" />
         </Text3D>
@@ -95,18 +71,7 @@ function RegularText({ text, position }: { text: string; position: Vector3 }) {
 
 function LightText({ text, position }: { text: string; position: Vector3 }) {
     return (
-        <Text3D
-            position={position}
-            height={0.01}
-            size={SMALL_FONT_SIZE}
-            font="/fonts/Mono_Light.json"
-            // curveSegments={32}
-            // bevelEnabled
-            // bevelSize={0.001}
-            // bevelThickness={0.001}
-            // lineHeight={0.5}
-            // letterSpacing={-0.06}
-        >
+        <Text3D position={position} height={0.01} size={SMALL_FONT_SIZE} font="/fonts/Mono_Light.json">
             {text}
             <meshBasicMaterial color="#000000" />
         </Text3D>
@@ -115,25 +80,63 @@ function LightText({ text, position }: { text: string; position: Vector3 }) {
 
 const leftControls = [
     {
-        label: '1',
+        group: '1',
+        items: [
+            {
+                label: '1',
+            },
+            {
+                label: '2',
+            },
+            {
+                label: '3',
+            },
+        ],
     },
     {
-        label: '2',
-    },
-    {
-        label: '3',
+        group: '2',
+        items: [
+            {
+                label: '4',
+            },
+            {
+                label: '5',
+            },
+            {
+                label: '6',
+            },
+        ],
     },
 ];
 
 const rightControls = [
     {
-        label: 'A',
+        group: 'A',
+        items: [
+            {
+                label: 'A',
+            },
+            {
+                label: 'B',
+            },
+            {
+                label: 'C',
+            },
+        ],
     },
     {
-        label: 'B',
-    },
-    {
-        label: 'C',
+        group: 'B',
+        items: [
+            {
+                label: 'D',
+            },
+            {
+                label: 'E',
+            },
+            {
+                label: 'F',
+            },
+        ],
     },
 ];
 
@@ -142,54 +145,23 @@ export function World() {
     const leftShape = createLeftShape();
     const rightShape = createRightShape();
 
+    const GAP = 0.1;
+    const controls = leftControls[0].items.length;
+    const LEFT_X = controls * WIDTH + GAP * (controls - 1);
+    const OFFSET_X = 0.3;
+
+    console.log(LEFT_X);
+
     useEffect(() => {
         const controls = cameraControlsRef.current;
 
         if (!controls) return;
 
-        // controls.setLookAt(1, 1.5, 2, 0, 1, 0, false);
-
         controls.setLookAt(0, 2, 12, 0, 2, 0, false);
-
-        requestAnimationFrame(() => {
-            // controls.setLookAt(0, 2.5, 6, 0, 2.5, 0, true);
-            // controls.setLookAt(0, 2.5, 6, 0, 2.5, 0, false);
-        });
     }, []);
 
     const leftControlsRef = useRef<Group>(null);
     const rightControlsRef = useRef<Group>(null);
-    const { size } = useThree();
-
-    const offsetX = 20;
-    const offsetY = 20;
-    const offsetZ = 0;
-
-    // useEffect(() => {
-    //     const leftGroup = leftControlsRef.current;
-    //     const rightGroup = rightControlsRef.current;
-
-    //     if (!leftGroup || !rightGroup) return;
-
-    //     // console.log('size:', size);
-
-    //     function update() {
-    //         // const x = -3.75;
-    //         const x = size.width / 270;
-    //         const y = -0.5;
-    //         const z = 0;
-
-    //         leftGroup?.position.set(-x, y, z);
-    //         leftGroup?.rotation.set(0, 0.25, 0);
-
-    //         rightGroup?.position.set(x - (rightControls.length - .3), y, z);
-    //         rightGroup?.rotation.set(0, -0.25, 0);
-    //     }
-
-    //     update();
-    // }, [size, offsetX, offsetY, offsetZ]);
-
-    const x = 3.3;
 
     return (
         <>
@@ -216,51 +188,53 @@ export function World() {
                 <meshBasicMaterial color="#ff0000" />
             </mesh>
 
-            {/* Left Controls */}
-            {/* <PivotControls offset={[-0.1 - 0.5, 0, 0]}> */}
-            <group ref={leftControlsRef} position={[-x, 0, 0]} rotation={[0, 0.25, 0]}>
-                <group position={[-x, 0, 0]}>
-                    {leftControls.map(({ label }, index) => {
-                        const x = index > 0 ? index * WIDTH + 0.1 * index : 0;
-                        const y = index * 0.1;
+            <group ref={leftControlsRef} position={[-OFFSET_X, 0, 0]} rotation={[-0.25, 0.25, 0.125]}>
+                {leftControls.map(({ items }, index) => {
+                    return (
+                        <group key={index} position={[-LEFT_X, (HEIGHT + GAP) * index, 0]}>
+                            {items.map(({ label }, index) => {
+                                const x = index > 0 ? index * WIDTH + GAP * index : 0;
+                                const y = index * GAP;
 
-                        return (
-                            <group key={index} position={[x, y, 0]}>
-                                <mesh>
-                                    <shapeGeometry args={[leftShape]} />
-                                    <meshBasicMaterial transparent opacity={0} depthWrite={false} />{' '}
-                                    <Edges linewidth={2} threshold={15} color="#000000" />
-                                </mesh>
-                                <BoldText text={label} position={CENTER} />
-                            </group>
-                        );
-                    })}
-                </group>
+                                return (
+                                    <group key={index} position={[x, y, 0]}>
+                                        <mesh>
+                                            <shapeGeometry args={[leftShape]} />
+                                            <meshBasicMaterial transparent opacity={0} depthWrite={false} />{' '}
+                                            <Edges linewidth={2} threshold={15} color="#000000" />
+                                        </mesh>
+                                        <BoldText text={label} position={CENTER} />
+                                    </group>
+                                );
+                            })}
+                        </group>
+                    );
+                })}
             </group>
-            {/* </PivotControls> */}
 
-            {/* Right Controls */}
-            {/* <PivotControls offset={[0.1 + 0.5, 0, 0]}> */}
-            <group ref={rightControlsRef} position={[0, 0, 0]} rotation={[0, -0.25, 0]}>
-                <group position={[0, 0, 0]}>
-                    {rightControls.map(({ label }, index) => {
-                        const x = index > 0 ? index * WIDTH + 0.1 * index : 0;
-                        const y = (rightControls.length - 1 - index) * 0.1;
+            <group ref={rightControlsRef} position={[OFFSET_X, 0, 0]} rotation={[-0.25, -0.25, -0.125]}>
+                {rightControls.map(({ items }, index) => {
+                    return (
+                        <group key={index} position={[0, (HEIGHT + GAP) * index, 0]}>
+                            {items.map(({ label }, index) => {
+                                const x = index > 0 ? index * WIDTH + GAP * index : 0;
+                                const y = (rightControls.length - index) * GAP;
 
-                        return (
-                            <group key={index} position={[x, y, 0]}>
-                                <mesh>
-                                    <shapeGeometry args={[rightShape]} />
-                                    <meshBasicMaterial transparent opacity={0} depthWrite={false} />{' '}
-                                    <Edges linewidth={2} threshold={15} color="#000000" />
-                                </mesh>
-                                <BoldText text={label} position={CENTER} />
-                            </group>
-                        );
-                    })}
-                </group>
+                                return (
+                                    <group key={index} position={[x, y, 0]}>
+                                        <mesh>
+                                            <shapeGeometry args={[rightShape]} />
+                                            <meshBasicMaterial transparent opacity={0} depthWrite={false} />{' '}
+                                            <Edges linewidth={2} threshold={15} color="#000000" />
+                                        </mesh>
+                                        <BoldText text={label} position={CENTER} />
+                                    </group>
+                                );
+                            })}
+                        </group>
+                    );
+                })}
             </group>
-            {/* </PivotControls> */}
 
             <Grid
                 position={[0, 0, 0]}
