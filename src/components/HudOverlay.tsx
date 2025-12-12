179fa8d6f896d1bca2006d617205cbcaff70fd12
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-12-12 13:57:09 -0500

    feat: cap hud scale (WIP)

--- src/components/HudOverlay.tsx
+++ src/components/HudOverlay.tsx
@@ -5,7 +5,10 @@ import { HudCenterReadout } from '@/components/HudCenterReadout';
 import { HudRightReadout } from '@/components/HudRightReadout';
 import { HudLeftTools } from '@/components/HudLeftTools';
 import { HudProvider, useHud } from '@/providers/HudProvider';
-import { useEffect, type ReactNode } from 'react';
+import { useEffect, useRef, type ReactNode } from 'react';
+// import { useThree } from '@react-three/fiber';
+
+// const MAX_VIEWPORT_WIDTH = 25.759047956216584;
 
 function HudWrapper({ children }: { children: ReactNode }) {
     const { lockHud } = useHud();
@@ -14,21 +17,31 @@ function HudWrapper({ children }: { children: ReactNode }) {
 }
 
 export function HudOverlay({ onReady }: { onReady: () => void }) {
+    // const viewport = useThree((state) => state.viewport);
+    const scale = useRef<number>(1);
+
     useEffect(() => {
+        // const { width, height } = viewport;
+
+        // console.log(width, height);
+
         const id = requestAnimationFrame(() => onReady?.());
 
         return () => cancelAnimationFrame(id);
+        // }, [viewport]);
     }, []);
 
     return (
         <HudProvider>
             <HudWrapper>
                 <PerspectiveCamera makeDefault position={[0, 2, 11.8]} fov={25} />
-                <HudLeftTools />
-                <HudFullKeyboard />
-                <HudLeftReadout />
-                <HudCenterReadout />
-                <HudRightReadout />
+                <group scale={scale.current}>
+                    <HudLeftTools />
+                    <HudFullKeyboard />
+                    <HudLeftReadout />
+                    <HudCenterReadout />
+                    <HudRightReadout />
+                </group>
             </HudWrapper>
         </HudProvider>
     );
