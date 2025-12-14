f9f1ee4cb602d23560f5192b09e28c2c97c78a20
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-12-14 17:41:42 -0500

    Update HudLeftReadout.tsx

--- src/components/HudLeftReadout.tsx
+++ src/components/HudLeftReadout.tsx
@@ -3,12 +3,19 @@ import { FONT } from '@/lib/constants';
 
 export function HudLeftReadout() {
     return (
-        <group position={[-3.65, 4.25, 0]}>
-            <Text3D position={[0, 0, 0]} height={0.001} size={0.28} font={FONT}>
-                grid OO 00
+        <group position={[-3.65, 4.35, 0]}>
+            <Text3D position={[0, 0, 0]} height={0.001} size={0.2} font={FONT}>
+                dock 2035
                 <meshBasicMaterial transparent opacity={0} depthWrite={false} />
                 <Edges linewidth={2} threshold={15} color="#000000" />
             </Text3D>
+            <group position={[0, -0.2, 0]}>
+                <Text3D position={[0, 0, 0]} height={0.001} size={0.1} font={FONT}>
+                    [ insert ]
+                    <meshBasicMaterial transparent opacity={0} depthWrite={false} />
+                    <meshBasicMaterial color="#000000" />
+                </Text3D>
+            </group>
         </group>
     );
 }
