b3b9fcb83422e2e405b69dad0ce1e090696ef893
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-12-22 21:46:55 -0500

    feat: hud (WIP)

--- src/components/HudOverlay.tsx
+++ src/components/HudOverlay.tsx
@@ -1,7 +1,6 @@
 import { Hud, PerspectiveCamera } from '@react-three/drei';
 import { HudLeftReadout } from './HudLeftReadout';
 import { HudFullKeyboard } from '@/components/HudFullKeyboard';
-// import { HudCenterReadout } from '@/components/HudCenterReadout';
 import { HudRightReadout } from '@/components/HudRightReadout';
 import { HudLeftTools } from '@/components/HudLeftTools';
 import { HudProvider, useHud } from '@/providers/HudProvider';
@@ -18,7 +17,7 @@ function HudWrapper({ children }: { children: ReactNode }) {
             {children}
         </Hud>
     ) : (
-        <group position={[0, -2, 0]}>{children}</group>
+        <group position={[0, .75, 0]}>{children}</group>
     );
 }
 
@@ -39,7 +38,6 @@ export function HudOverlay({ onReady }: { onReady: () => void }) {
                     <HudCenterTools />
                     <HudFullKeyboard />
                     <HudLeftReadout />
-                    {/* <HudCenterReadout /> */}
                     <HudRightReadout />
                     <EventVisualizer />
                 </group>
@@ -47,3 +45,81 @@ export function HudOverlay({ onReady }: { onReady: () => void }) {
         </HudProvider>
     );
 }
+
+// import { Scene, Camera } from 'three';
+// import { PerspectiveCamera } from '@react-three/drei';
+// import { HudLeftReadout } from './HudLeftReadout';
+// import { HudFullKeyboard } from '@/components/HudFullKeyboard';
+// import { HudRightReadout } from '@/components/HudRightReadout';
+// import { HudLeftTools } from '@/components/HudLeftTools';
+// import { HudProvider } from '@/providers/HudProvider';
+// import { useEffect, useState } from 'react';
+// import { HudCenterTools } from './HudCenterTools';
+// import { EventVisualizer } from './EventVisualizer';
+// import { useFrame, useThree, createPortal } from '@react-three/fiber';
+
+// const RENDER_PRIORITY: number = 2;
+
+// type RenderHudProps = {
+//     defaultScene: Scene;
+//     defaultCamera: Camera;
+//     renderPriority?: number;
+// };
+
+// function RenderHud({ defaultScene, defaultCamera }: RenderHudProps) {
+//     const { gl, scene, camera } = useThree();
+//     let oldCLear;
+
+//     useFrame(() => {
+//         oldCLear = gl.autoClear;
+
+//         if (RENDER_PRIORITY === 1) {
+//             gl.autoClear = true;
+
+//             gl.render(defaultScene, defaultCamera);
+//         }
+
+//         gl.autoClear = false;
+
+//         // gl.clearDepth();
+//         gl.render(scene, camera);
+
+//         gl.autoClear = oldCLear;
+//     }, RENDER_PRIORITY);
+
+//     return <group scale={1} onPointerOver={() => null} />;
+// }
+
+// export function HudOverlay({ onReady }: { onReady: () => void }) {
+//     const { scene: defaultScene, camera: defaultCamera } = useThree();
+//     const [hudScene] = useState(() => new Scene());
+
+//     useEffect(() => {
+//         const id = requestAnimationFrame(() => onReady?.());
+
+//         return () => cancelAnimationFrame(id);
+//     }, []);
+
+//     return (
+//         <HudProvider>
+//             {createPortal(
+//                 <>
+//                     <PerspectiveCamera makeDefault position={[0, 2, 11.8]} fov={25} />
+//                     <HudLeftTools />
+//                     <HudCenterTools />
+//                     <HudFullKeyboard />
+//                     <HudLeftReadout />
+//                     <HudRightReadout />
+//                     <EventVisualizer />
+//                     <RenderHud
+//                         defaultScene={defaultScene}
+//                         defaultCamera={defaultCamera}
+//                         renderPriority={RENDER_PRIORITY}
+//                     />
+//                 </>,
+//                 hudScene,
+//                 { events: { priority: RENDER_PRIORITY } }
+//             )}
+//         </HudProvider>
+//     );
+// }
